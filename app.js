ac19f9e57d5cfcfb78cee14947b64c959354d948
ELIJAHKUNGU <elijahkungu100@gmail.com>
2021-06-12 11:19:38 +0300

    second commit

--- app.js
+++ app.js
@@ -282,12 +282,12 @@ $(document).ready(function() {
 
 var $grid = $(".grid").isotope({
   itemSelector: ".grid-item",
-  layoutMode: "fitRows",
 });
 
 // filter items on button click
 $(".button-group").on("click", "button", function () {
   var filterValue = $(this).attr("data-filter");
+  console.log(filterValue);
 
   $grid.isotope({ filter: filterValue });
 });
